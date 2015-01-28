d646e6eb797278edfb7ccb19426ac819c61b72e2
tpakhoa1996 <m48554517@gmail.com>
2015-01-28 22:58:22 +0700

    second

---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from mysite import view
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^polls/', include('polls.urls')),
+    url(r'^$', view.index, name = 'home'),
+)
